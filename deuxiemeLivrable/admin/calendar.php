19e3114086f6f7361a3f9df16b1dcd39991cd173
fallou-dev <81333040+fallou-dev@users.noreply.github.com>
2022-03-31 07:52:33 +0000

    Update calendar.php

--- deuxiemeLivrable/admin/calendar.php
+++ deuxiemeLivrable/admin/calendar.php
@@ -233,7 +233,7 @@ if (isset($_SESSION['username'])){
                                                
                                             </div>
                                             <div class="account-dropdown__footer">
-                                                <a href="logout.php">
+                                                <a href="../logout.php">
                                                     <i class="zmdi zmdi-power"></i>Logout</a>
                                             </div>
                                         </div>
